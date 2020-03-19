9fcad53f506a2390f570c198e1816a530d586bf2
esoft11 - Daniel Barreto <dandpb@gmail.com>
2020-03-18 21:39:37 -0300

    mail template engine

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'e83984305d1665',
+    pass: '0a66baa53f1843',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
